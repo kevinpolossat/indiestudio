ae5bfb2a68f5d162841be6adf100daef03f8db47
Mickaël Leclerc <mickael.leclerc@epitech.eu>
2017-05-12 10:11:25 +0200

    Regenerated map

--- src/Map.cpp
+++ src/Map.cpp
@@ -131,6 +131,7 @@ int main()
                    walls.push_back(Cell(irr::core::vector3df(x, 0, y), irr::core::vector3df(0, 0, 0), "DefaultGround"));
                    break;
                case 3:
+                   walls.push_back(Cell(irr::core::vector3df(x, 0, y), irr::core::vector3df(0, 0, 0), "DefaultGround"));
                    spawns.push_back(Cell(irr::core::vector3df(x, 1, y), irr::core::vector3df(0, 0, 0), ""));
                    break;
                default:
