9512a75f3662a3cf87b65fe7ae91990ef76156dd
kevin <kevin.polossat@epitech.eu>
2017-05-15 11:17:20 +0200

    Bomberman: add BomberMan class

---
+++ src/BomberMan.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kevin on 15/05/17.
+//
+
+#include "BomberMan.hh"
+
+BomberMan::BomberMan() {
+
+}
+
+BomberMan::~BomberMan() {
+
+}
---
+++ src/BomberMan.hh
@@ -0,0 +1,16 @@
+//
+// Created by kevin on 15/05/17.
+//
+
+#ifndef INDIESTUDIO_BOMBERMAN_HH
+#define INDIESTUDIO_BOMBERMAN_HH
+
+
+class BomberMan {
+public:
+    BomberMan();
+    ~BomberMan();
+};
+
+
+#endif //INDIESTUDIO_BOMBERMAN_HH
