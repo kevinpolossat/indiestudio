c76d3d6010371506a0f81f1661fa23df938175ed
kevin <kevin.polossat@epitech.eu>
2017-05-15 11:16:00 +0200

    Settings: add settings class

---
+++ src/Settings.hh
@@ -0,0 +1,15 @@
+//
+// Created by kevin on 15/05/17.
+//
+
+#ifndef INDIESTUDIO_SETTINGS_HH
+#define INDIESTUDIO_SETTINGS_HH
+
+class Settings {
+public:
+    Settings();
+    ~Settings();
+};
+
+
+#endif //INDIESTUDIO_SETTINGS_HH
