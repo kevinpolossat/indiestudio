c890a4092b2326ccdc03ad439f01edaa674462ef
kevin <kevin.polossat@epitech.eu>
2017-05-22 20:42:49 +0200

    cmake: add boost_python dependencies

--- src/Referee.hh
+++ src/Referee.hh
@@ -7,6 +7,8 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
+
 #include "Bomb.hh"
 #include "PowerUp.hh"
 #include "Character.hh"
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <boost/python.hpp>
 #include "Game.hh"
 
 int main(void) {
@@ -5,44 +6,3 @@ int main(void) {
 
 	return bomberman.run();
 }
-/*
-#include <iostream>
-#include <vector>
-#include <memory>
-#include "Scene.hh"
-#include "MenuMainPage.hh"
-#include "MenuSettingsPage.hh"
-#include "EventReceiver.hh"
-
-int main() {
-
-    EventReceiver receiver;
-
-    irr::IrrlichtDevice *device = irr::createDevice(irr::video::EDT_OPENGL, irr::core::dimension2d<irr::u32>(1920, 1080), 16, false, false, false, &receiver);
-    if (!device)
-        return 1;
-
-    device->setWindowCaption(L"BOMBERBOOM");
-
-    std::vector<std::unique_ptr<Scene>>    Scenes;
-    int menuState = 0;
-
-    Scenes.push_back(std::make_unique<MenuMainPage>(device));
-    Scenes.push_back(std::make_unique<MenuSettingsPage>(device));
-    Scenes.push_back(std::make_unique<MenuSettingsPage>(device));
-    if (!Scenes[menuState]->setScene(device)) {
-        return 0;
-    }
-    while (device->run()) {
-        int rtn = Scenes[menuState]->refresh(device, &menuState, &receiver);
-        if (!rtn) {
-            break;
-        } else if (rtn == 1) {
-            if (!Scenes[menuState]->setScene(device))
-                return 0;
-        }
-    }
-    device->drop();
-    return 0;
-}
-*/
