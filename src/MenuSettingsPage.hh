3f4ff34c966f989915fca5e17a330e8663989c33
Charles Fraïssé <charles.fraisse@epitech.eu>
2017-05-24 14:05:56 +0200

    MenuSettingsPage Keymangement UI

--- src/MenuSettingsPage.hh
+++ src/MenuSettingsPage.hh
@@ -21,9 +21,30 @@ private:
     int                                 _bHeight;
     int                                 _bWidth;
     irr::gui::IGUIButton                *_bg;
-    irr::u32                            _time;
-    irr::gui::IGUIStaticText            *_back;
-    irr::gui::IGUIStaticText            *_reset;
+    irr::gui::IGUIButton                *_back;
+    irr::gui::IGUIButton                *_reset;
+    irr::gui::IGUIButton                *_player1Bomb;
+    irr::gui::IGUIButton                *_player1BombKey;
+    irr::gui::IGUIButton                *_player2Bomb;
+    irr::gui::IGUIButton                *_player2BombKey;
+    irr::gui::IGUIButton                *_player1Right;
+    irr::gui::IGUIButton                *_player1RightKey;
+    irr::gui::IGUIButton                *_player2Right;
+    irr::gui::IGUIButton                *_player2RightKey;
+    irr::gui::IGUIButton                *_player1Left;
+    irr::gui::IGUIButton                *_player1LeftKey;
+    irr::gui::IGUIButton                *_player2Left;
+    irr::gui::IGUIButton                *_player2LeftKey;
+    irr::gui::IGUIButton                *_player1Down;
+    irr::gui::IGUIButton                *_player1DownKey;
+    irr::gui::IGUIButton                *_player2Down;
+    irr::gui::IGUIButton                *_player2DownKey;
+    irr::gui::IGUIButton                *_player1Up;
+    irr::gui::IGUIButton                *_player1UpKey;
+    irr::gui::IGUIButton                *_player2Up;
+    irr::gui::IGUIButton                *_player2UpKey;
+    irr::gui::IGUIButton                *_player1Title;
+    irr::gui::IGUIButton                *_player2Title;
 
 
     bool                    isMouseOnBack() const;
