aa6386ba8168391470c494d906ab658503b79a17
kevin <kevin.polossat@epitech.eu>
2017-05-29 15:35:45 +0200

    Particles: Explosion are now built with an vector3f as a position where it should be displayed

--- src/Particles/Explosion.hh
+++ src/Particles/Explosion.hh
@@ -8,12 +8,15 @@
 
 class Explosion {
 public:
-    explicit Explosion();
+    explicit Explosion(irr::core::vector3df const & pos);
+
     Explosion(Explosion const & other)                  = delete;
     Explosion(Explosion && other)                       = delete;
     Explosion & operator = (Explosion const & other)    = delete;
 
     ~Explosion();
+private:
+    irr::scene::IParticleSystemSceneNode *_ps;
 };
 
 
