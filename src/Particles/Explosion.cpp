f0c23a225a8bd4c16b912e161ff079d5e5e5f13c
kevin <kevin.polossat@epitech.eu>
2017-05-31 18:43:58 +0200

    indi(Particles): add Particle

--- src/Particles/Explosion.cpp
+++ src/Particles/Explosion.cpp
@@ -7,28 +7,30 @@
 #include "Explosion.hh"
 
 Explosion::Explosion(irr::core::vector3df const & pos) {
-    _ps = ResourceManager::sceneManager()->addParticleSystemSceneNode(false);
+    _ps = std::shared_ptr<irr::scene::IParticleSystemSceneNode>(ResourceManager::sceneManager()->addParticleSystemSceneNode(false),
+        [](irr::scene::IParticleSystemSceneNode *_ips) {
+            _ips->remove();
+        }
+    );
     irr::scene::IParticleEmitter* em = _ps->createSphereEmitter(irr::core::vector3df(0.0f, 0.0f, 0.0f),
                                                           1.0f,
                                                           irr::core::vector3df(0.0f, 0.2f, 0.0f),
-                                                          3000, 4000,
+                                                          300, 400,
                                                           irr::video::SColor(255, 0, 230, 118),
                                                           irr::video::SColor(255, 0, 200, 83),
-                                                          150, 250, 360,
-                                                          irr::core::dimension2df(5.f, 5.f),
-                                                          irr::core::dimension2df(6.f, 6.f));
-
-
+                                                          15, 25, 360,
+                                                          irr::core::dimension2df(0.2f, 0.2f),
+                                                          irr::core::dimension2df(3.f, 3.f));
     _ps->setEmitter(em);
     em->drop();
-    irr::scene::IParticleAffector* paf = _ps->createFadeOutParticleAffector(irr::video::SColor(0, 255, 255, 255), 250);
+    irr::scene::IParticleAffector *paf = _ps->createFadeOutParticleAffector(irr::video::SColor(0, 255, 255, 255), 250);
     _ps->addAffector(paf);
     paf->drop();
-    irr::scene::IParticleAffector * saf =  _ps->createScaleParticleAffector(irr::core::dimension2df(0.f, 0.f));
+    irr::scene::IParticleAffector *saf =  _ps->createScaleParticleAffector(irr::core::dimension2df(0.f, 0.f));
     _ps->addAffector(saf);
     saf->drop();
     _ps->setPosition(pos);
-    _ps->setScale(irr::core::vector3df(2,2,2));
+    _ps->setScale(irr::core::vector3df(1, 1, 1));
     _ps->setMaterialFlag(irr::video::EMF_LIGHTING, false);
     _ps->setMaterialFlag(irr::video::EMF_ZWRITE_ENABLE, false);
     _ps->setMaterialTexture(0, ResourceManager::videoDriver()->getTexture("./dependencies/irrlicht/Source/Irrlicht/media/particlegreen.jpg"));
@@ -36,5 +38,17 @@ Explosion::Explosion(irr::core::vector3df const & pos) {
 }
 
 Explosion::~Explosion() {
-    _ps->remove();
+}
+
+Explosion::Explosion(Explosion const &other) {
+    _ps = other._ps;
+}
+
+Explosion::Explosion(Explosion &&other) {
+    _ps = other._ps;
+}
+
+Explosion &Explosion::operator=(Explosion const &other) {
+    _ps = other._ps;
+    return *this;
 }
--- src/Particles/Explosion.hh
+++ src/Particles/Explosion.hh
@@ -5,18 +5,20 @@
 #ifndef INDIESTUDIO_EXPLOSION_HH
 #define INDIESTUDIO_EXPLOSION_HH
 
+#include <irrlicht.h>
+#include <memory>
 
 class Explosion {
 public:
     explicit Explosion(irr::core::vector3df const & pos);
 
-    Explosion(Explosion const & other)                  = delete;
-    Explosion(Explosion && other)                       = delete;
-    Explosion & operator = (Explosion const & other)    = delete;
+    Explosion(Explosion const & other);
+    Explosion(Explosion && other);
+    Explosion & operator = (Explosion const & other);
 
     ~Explosion();
 private:
-    irr::scene::IParticleSystemSceneNode *_ps;
+    std::shared_ptr<irr::scene::IParticleSystemSceneNode> _ps;
 };
 
 
