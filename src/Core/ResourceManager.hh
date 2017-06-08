bc1008f6a679b5dfed8329b8e8ea0710628a7656
kevin <kevin.polossat@epitech.eu>
2017-06-08 15:00:12 +0200

    ResourceManager(sounds): add loading sound

--- src/Core/ResourceManager.hh
+++ src/Core/ResourceManager.hh
@@ -8,6 +8,7 @@
 #include <unordered_map>
 #include <memory>
 #include <irrlicht.h>
+#include <SFML/Audio/SoundBuffer.hpp>
 #include "EventHandler.hh"
 
 class ResourceManager {
@@ -22,12 +23,17 @@ public:
     static std::shared_ptr<irr::gui::IGUIEnvironment>   guiEnvironment();
     static EventHandler const &                         eventHandler();
 
-    static irr::scene::IAnimatedMesh   *getAnimatedMesh(std::string const & name);
-    static void                        loadAnimatedMesh(std::string const & name, std::string const &path = "./assets/");
+    static irr::scene::IAnimatedMesh    *getAnimatedMesh(std::string const & name);
+    static void                         loadAnimatedMesh(std::string const & name, std::string const &path = "./assets/");
+
+    static void                         loadSound(std::string const & name, std::string const &path = "./assets/");
+    static sf::SoundBuffer const &      getSound(std::string const name);
+
 private:
     EventHandler                                                    _handler;
     std::shared_ptr<irr::IrrlichtDevice>                            _device;
     std::unordered_map<std::string, irr::scene::IAnimatedMesh *>    _animatedMesh;
+    std::unordered_map<std::string, sf::SoundBuffer>                _sounds;
 
 private:
     explicit ResourceManager(irr::video::E_DRIVER_TYPE driverType = irr::video::EDT_OPENGL,
@@ -47,6 +53,10 @@ private:
 
     irr::scene::IAnimatedMesh   *getAnimatedMesh_impl(std::string const & name) const;
     void                        loadAnimatedMesh_impl(std::string const & name, std::string const &path = "./assets/");
+
+    void                        loadSound_impl(std::string const & name, std::string const &path = "./assets/");
+    sf::SoundBuffer const &     getSound_impl(std::string const name);
+
     static ResourceManager &    instance();
 };
 
