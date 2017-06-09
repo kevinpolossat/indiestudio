6768e14476f0496b22a64d0445b781bb7bdd2e48
kevin <kevin.polossat@epitech.eu>
2017-06-09 22:27:13 +0200

    feat/animation(add): update all ieffects

--- src/Animation/SpecialEffectManager.cpp
+++ src/Animation/SpecialEffectManager.cpp
@@ -15,6 +15,7 @@ SpecialEffectManager::~SpecialEffectManager() {
 
 void SpecialEffectManager::refresh() {
     _effects.erase(std::remove_if(_effects.begin(), _effects.end(), isOver), _effects.end());
+    std::for_each(_effects.begin(), _effects.end(), [](std::unique_ptr<IEffect> const &e){ e->update(); });
 }
 
 bool SpecialEffectManager::isOver(const std::unique_ptr<IEffect> &x) {
