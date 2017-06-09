02d8b8612f7c8fbb9726da707e1d87ce3969f010
kevin <kevin.polossat@epitech.eu>
2017-06-09 12:01:50 +0200

    settings(add getter for keymap)

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -7,25 +7,24 @@
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
 #include <irrlicht.h>
+#include <iostream>
 #include "Settings.hh"
 
 Settings::Settings() {
     std::ifstream ifs("./saves/.settings");
     if (ifs.good()) {
         boost::archive::text_iarchive ia(ifs);
-        ia >> *this;
+        try {
+            ia >> *this;
+        }
+        catch (boost::archive::archive_exception const & ae) {
+            default_init();
+        }
         std::min(std::max(0, _music_volume), 10);
         std::min(std::max(0, _sound_volume), 10);
     }
     else {
-        _music_volume = 10;
-        _sound_volume = 10;
-        this->_keyMapP1 = { irr::EKEY_CODE::KEY_KEY_Z, irr::EKEY_CODE::KEY_KEY_D,
-                            irr::EKEY_CODE::KEY_KEY_S, irr::EKEY_CODE::KEY_KEY_Q,
-                            irr::EKEY_CODE::KEY_SPACE };
-        this->_keyMapP2 = { irr::EKEY_CODE::KEY_UP, irr::EKEY_CODE::KEY_RIGHT,
-                            irr::EKEY_CODE::KEY_DOWN, irr::EKEY_CODE::KEY_LEFT,
-                            irr::EKEY_CODE::KEY_END };
+        default_init();
     }
 }
 
@@ -42,6 +41,18 @@ Settings &Settings::instance() {
     return settings;
 }
 
+void Settings::default_init() {
+    _music_volume = 10;
+    _sound_volume = 10;
+    _keyMapP1 = { irr::EKEY_CODE::KEY_KEY_Z, irr::EKEY_CODE::KEY_KEY_D,
+                        irr::EKEY_CODE::KEY_KEY_S, irr::EKEY_CODE::KEY_KEY_Q,
+                        irr::EKEY_CODE::KEY_SPACE };
+    _keyMapP2 = { irr::EKEY_CODE::KEY_UP, irr::EKEY_CODE::KEY_RIGHT,
+                        irr::EKEY_CODE::KEY_DOWN, irr::EKEY_CODE::KEY_LEFT,
+                        irr::EKEY_CODE::KEY_END };
+
+}
+
 int &Settings::music_volume_impl() {
     return _music_volume;
 }
@@ -50,10 +61,27 @@ int &Settings::sound_volume_impl() {
     return _sound_volume;
 }
 
+std::array<irr::EKEY_CODE, 5> &Settings::keyMapP2_impl() {
+    return _keyMapP1;
+}
+
+std::array<irr::EKEY_CODE, 5> &Settings::keyMapP1_impl() {
+    return _keyMapP2;
+}
+
 int &Settings::music_volume() {
-    return Settings::instance()._music_volume;
+    return Settings::instance().music_volume_impl();
 }
 
 int &Settings::sound_volume() {
-    return Settings::instance()._sound_volume;
+    return Settings::instance().sound_volume_impl();
+}
+
+
+std::array<irr::EKEY_CODE, 5> &Settings::keyMapP1() {
+    return Settings::instance().keyMapP1_impl();
+}
+
+std::array<irr::EKEY_CODE, 5> &Settings::keyMapP2() {
+    return Settings::instance().keyMapP2_impl();
 }
--- src/Core/Settings.hh
+++ src/Core/Settings.hh
@@ -14,8 +14,10 @@ class Settings {
 public:
     ~Settings();
 
-    static int &music_volume();
-    static int &sound_volume();
+    static int                              &music_volume();
+    static int                              &sound_volume();
+    static std::array<irr::EKEY_CODE, 5>    &keyMapP1();
+    static std::array<irr::EKEY_CODE, 5>    &keyMapP2();
 
     template <class Archive>
     void serialize(Archive &ar, unsigned int const) {
@@ -32,14 +34,17 @@ private:
 
     Settings    &operator = (Settings const &other) = delete;
     static      Settings &instance();
+    void        default_init();
 
     int                             _music_volume;
     int                             _sound_volume;
     std::array<irr::EKEY_CODE, 5>   _keyMapP1;
     std::array<irr::EKEY_CODE, 5>   _keyMapP2;
 
-    int         &music_volume_impl();
-    int         &sound_volume_impl();
+    int                             &music_volume_impl();
+    int                             &sound_volume_impl();
+    std::array<irr::EKEY_CODE, 5>   &keyMapP1_impl();
+    std::array<irr::EKEY_CODE, 5>   &keyMapP2_impl();
 };
 
 
