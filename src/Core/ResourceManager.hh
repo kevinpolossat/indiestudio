0deb5db26cd181ac5e6ac88cc3beb718a904bbae
kevin <kevin.polossat@epitech.eu>
2017-06-13 13:52:26 +0200

    load(add): assimp loader

--- src/Core/ResourceManager.hh
+++ src/Core/ResourceManager.hh
@@ -9,7 +9,9 @@
 #include <memory>
 #include <irrlicht.h>
 #include <SFML/Audio/SoundBuffer.hpp>
+
 #include "EventHandler.hh"
+#include "IrrAssimp.h"
 
 class ResourceManager {
 
@@ -25,6 +27,7 @@ public:
 
     static irr::scene::IAnimatedMesh    *getAnimatedMesh(std::string const & name);
     static void                         loadAnimatedMesh(std::string const & name, std::string const &path = "./assets/");
+    static void                         loadAssimpMesh(std::string const & name, std::string const &path = "./assets/");
 #ifdef SOUND
     static void                         loadSound(std::string const & name, std::string const &path = "./assets/");
     static sf::SoundBuffer const &      getSound(std::string const & name);
@@ -36,7 +39,7 @@ private:
   #ifdef SOUND
     std::unordered_map<std::string, sf::SoundBuffer>                _sounds;
   #endif
-
+    IrrAssimp                                                       _assimpImporter;
 private:
     explicit ResourceManager(irr::video::E_DRIVER_TYPE driverType = irr::video::EDT_OPENGL,
                              irr::core::dimension2d<irr::u32> const &dim = irr::core::dimension2d<irr::u32>(1920, 1080),
@@ -51,11 +54,12 @@ private:
     std::shared_ptr<irr::video::IVideoDriver>       videoDriver_impl()      const;
     std::shared_ptr<irr::scene::ISceneManager>      sceneManager_impl()     const;
     std::shared_ptr<irr::gui::IGUIEnvironment>      guiEnvironment_impl()   const;
-    EventHandler const &                            eventHandler_impl();
+    EventHandler const &                            eventHandler_impl()     const;
 
     irr::scene::IAnimatedMesh   *getAnimatedMesh_impl(std::string const & name) const;
     void                        loadAnimatedMesh_impl(std::string const & name, std::string const &path = "./assets/");
-#ifdef SOUND
+    void                        loadAssimpMesh_impl(std::string const & name, std::string const &path = "./assets/");
+    #ifdef SOUND
     void                        loadSound_impl(std::string const & name, std::string const &path = "./assets/");
     sf::SoundBuffer const &     getSound_impl(std::string const & name);
 #endif
