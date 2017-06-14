0c638a98899928ab2ba596bfe24d3695a807fcf9
Vincent LELONG <vincent.lelong@epitech.eu>
2017-06-14 17:09:25 +0200

    fix: action

--- src/BomberMan/Player.cpp
+++ src/BomberMan/Player.cpp
@@ -39,23 +39,23 @@ void Player::move(EventHandler const & receiver, Referee & referee) {
     auto firstController = ResourceManager::eventHandler().getJoystick(ResourceManager::getControllers()[0]);
     if (!this->_isUsingCtrllr) {
         if (receiver.isKeyDown(this->_keyMap[0]) || firstController.Axis[1] < 0) {
-            referee.doAction(this->_id, Action::UP, 0);
+            referee.doAction(Action(this->_id, Action::UP, 0));
 //          moved = true;
         }
         if (receiver.isKeyDown(this->_keyMap[1]) || firstController.Axis[0] > 0) {
-            referee.doAction(this->_id, Action::RIGHT, 0);
+            referee.doAction(Action(this->_id, Action::RIGHT, 0));
 //          moved = true;
         }
         if (receiver.isKeyDown(this->_keyMap[2]) || firstController.Axis[1] > 0) {
-            referee.doAction(this->_id, Action::DOWN, 0);
+            referee.doAction(Action(this->_id, Action::DOWN, 0));
 //          moved = true;
         }
         if (receiver.isKeyDown(this->_keyMap[3]) || firstController.Axis[0] < 0) {
-            referee.doAction(this->_id, Action::LEFT, 0);
+            referee.doAction(Action(this->_id, Action::LEFT, 0));
 //        moved = true;
         }
         if (receiver.isKeyDown(this->_keyMap[4]) || firstController.ButtonStates == 2) {
-            referee.doAction(this->_id, Action::BOMB, 0);
+            referee.doAction(Action(this->_id, Action::BOMB, 0));
         }
 //        if (moved) {
 //            if (_anim != RUN) {
