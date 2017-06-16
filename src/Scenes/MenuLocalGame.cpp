48326304fd4b77258f534eabc443b3f4b0b49d60
kevin <kevin.polossat@epitech.eu>
2017-06-16 19:55:42 +0200

    MenuLocalGame is now compliance with assets dir

--- src/Scenes/MenuLocalGame.cpp
+++ src/Scenes/MenuLocalGame.cpp
@@ -26,66 +26,66 @@ MenuLocalGame::setScene() {
     _frame = 0;
     this->unsetScene();
     this->_bg = ResourceManager::device()->getGUIEnvironment()->addButton( irr::core::rect<irr::s32>(0, 0, 1920, 1080), 0, -1, NULL);
-    this->_bg->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/BG.png"));
+    this->_bg->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "BG.png").c_str()));
     this->_bg->setUseAlphaChannel(true);
     this->_bg->setDrawBorder(false);
     this->_bg->setEnabled(false);
 
     this->_back = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(10, 10, 250 + 10, 60 + 10), 0, -1, NULL);
-    this->_back->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/Fonts/Back_250x60.png"));
+    this->_back->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "Fonts/Back_250x60.png").c_str()));
     this->_back->setUseAlphaChannel(true);
     this->_back->setDrawBorder(false);
 
     this->_confirm = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(960 - 250, 1080 - 100 - 40, 960 + 250, 1080 - 20 - 40), 0, -1, NULL);
-    this->_confirm->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/Fonts/Confirm_500x80.png"));
+    this->_confirm->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "Fonts/Confirm_500x80.png").c_str()));
     this->_confirm->setUseAlphaChannel(true);
     this->_confirm->setDrawBorder(false);
 
 
     this->P1Picture = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(480 - 300, 287.5 - 150 - 40, 480 + 300, 287.5 + 150 - 40), 0, -1, NULL);
-    this->P1Picture->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/player_blue.png"));
+    this->P1Picture->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "player_blue.png").c_str()));
     this->P1Picture->setUseAlphaChannel(true);
     this->P1Picture->setDrawBorder(false);
     this->P1Picture->setEnabled(false);
 
     this->P1Role = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(480 - 300, 287.5 + 150 - 40 + 10, 480 + 300, 287.5 + 150 - 40 + 80), 0, -1, NULL);
-    this->P1Role->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/Fonts/Player1_600x70.png"));
+    this->P1Role->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "Fonts/Player1_600x70.png").c_str()));
     this->P1Role->setUseAlphaChannel(true);
     this->P1Role->setDrawBorder(false);
     this->P1Role->setEnabled(false);
 
 
     this->P2Picture = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(1440 - 300, 287.5 - 150 - 40, 1440 + 300, 287.5 + 150 - 40), 0, -1, NULL);
-    this->P2Picture->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/player_orange.png"));
+    this->P2Picture->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "player_orange.png").c_str()));
     this->P2Picture->setUseAlphaChannel(true);
     this->P2Picture->setDrawBorder(false);
     this->P2Picture->setEnabled(false);
 
     this->P2Role = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(1440 - 300, 287.5 + 150 - 40 + 10, 1440 + 300, 287.5 + 150 - 40 + 80), 0, -1, NULL);
-    this->P2Role->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/Fonts/Computer_600x70.png"));
+    this->P2Role->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "Fonts/Computer_600x70.png").c_str()));
     this->P2Role->setUseAlphaChannel(true);
     this->P2Role->setDrawBorder(false);
     this->P2Role->setEnabled(false);
 
     this->P2RoleLeft = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(1440 - 300 - 90, 287.5 + 150 - 40 + 10, 1440 - 300 - 10, 287.5 + 150 - 40 + 80), 0, -1, NULL);
-    this->P2RoleLeft->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/Fonts/LeftArrow_70x70.png"));
+    this->P2RoleLeft->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "Fonts/LeftArrow_70x70.png").c_str()));
     this->P2RoleLeft->setUseAlphaChannel(true);
     this->P2RoleLeft->setDrawBorder(false);
 
     this->P2RoleRight = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(1440 + 300 + 10, 287.5 + 150 - 40 + 10, 1440 + 300 + 90, 287.5 + 150 - 40 + 80), 0, -1, NULL);
-    this->P2RoleRight->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/Fonts/RightArrow_70x70.png"));
+    this->P2RoleRight->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "Fonts/RightArrow_70x70.png").c_str()));
     this->P2RoleRight->setUseAlphaChannel(true);
     this->P2RoleRight->setDrawBorder(false);
 
 
     this->P3Picture = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(480 - 300, 722.5 - 150 - 40, 480 + 300, 722.5 + 150 - 40), 0, -1, NULL);
-    this->P3Picture->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/player_green.png"));
+    this->P3Picture->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "player_green.png").c_str()));
     this->P3Picture->setUseAlphaChannel(true);
     this->P3Picture->setDrawBorder(false);
     this->P3Picture->setEnabled(false);
 
     this->P3Role = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(480 - 300, 722.5 + 150 - 40 + 10, 480 + 300, 722.5 + 150 - 40 + 80), 0, -1, NULL);
-    this->P3Role->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/Fonts/Computer_600x70.png"));
+    this->P3Role->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "Fonts/Computer_600x70.png").c_str()));
     this->P3Role->setUseAlphaChannel(true);
     this->P3Role->setDrawBorder(false);
     this->P3Role->setEnabled(false);
@@ -93,13 +93,13 @@ MenuLocalGame::setScene() {
 
     this->P4Picture = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(1440 - 300, 722.5 - 150 - 40, 1440 + 300, 722.5 + 150 - 40), 0, -1, NULL);
     this->P4Picture->setEnabled(false);
-    this->P4Picture->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/player_purple.png"));
+    this->P4Picture->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "player_purple.png").c_str()));
     this->P4Picture->setUseAlphaChannel(true);
     this->P4Picture->setDrawBorder(false);
     this->P4Picture->setEnabled(false);
 
     this->P4Role = ResourceManager::device()->getGUIEnvironment()->addButton(irr::core::rect<irr::s32>(1440 - 300, 722.5 + 150 - 40 + 10, 1440 + 300, 722.5 + 150 - 40 + 80), 0, -1, NULL);
-    this->P4Role->setImage(ResourceManager::device()->getVideoDriver()->getTexture("./assets/Fonts/Computer_600x70.png"));
+    this->P4Role->setImage(ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "Fonts/Computer_600x70.png").c_str()));
     this->P4Role->setUseAlphaChannel(true);
     this->P4Role->setDrawBorder(false);
     this->P4Role->setEnabled(false);
@@ -133,11 +133,11 @@ MenuLocalGame::refresh(int &menuState) {
             this->_time = now;
             if (this->_isP2IA) {
                 this->P2Role->setImage(
-                        ResourceManager::device()->getVideoDriver()->getTexture("./assets/Fonts/Player2_600x70.png"));
+                        ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "Fonts/Player2_600x70.png").c_str()));
                 this->_isP2IA = false;
             } else {
                 this->P2Role->setImage(
-                        ResourceManager::device()->getVideoDriver()->getTexture("./assets/Fonts/Computer_600x70.png"));
+                        ResourceManager::device()->getVideoDriver()->getTexture((ResourceManager::assets_rela + "Fonts/Computer_600x70.png").c_str()));
                 this->_isP2IA = true;
             }
         }
